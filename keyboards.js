34aed3b9f12b1292280d1f82daba6e3c71d3a7d7
Werwllleh <allvxm4@gmail.com>
2023-02-13 15:26:53 +0300

    update

--- keyboards.js
+++ keyboards.js
@@ -8,7 +8,7 @@ module.exports = {
 	menu: {
 		reply_markup: {
 			keyboard: [
-				[{ text: 'Категории' }],
+				[{ text: 'Направления деятельности' }],
 				[{ text: 'Социальные сети' }],
 				[{ text: 'Сотрудничество' }],
 			],
